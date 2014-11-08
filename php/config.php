55e12de0a3698bb3b80c2af2306b535a0cf85a44
alexisduque <alexisd61@gmail.com>
2014-11-08 14:30:46 +0100

    First commit

---
+++ php/config.php
@@ -0,0 +1,14 @@
+<?
+	//define the receiver of the email
+	define('TO_EMAIL','alexisd61@gmail.com');
+	
+	//define the subject of the email
+	define('SUBJECT','Contact from your website');	
+	
+	// Messages
+	define('MSG_INVALID_NAME','Veuillez indiquer votre nom.');
+	define('MSG_INVALID_EMAIL','Veuillez indiquer votre e-mail.');
+	define('MSG_INVALID_MESSAGE','Veuillez ecrire votre message.');
+	define('MSG_SEND_ERROR','Désolé, impossible d'envoyer votre message.');
+
+?>
